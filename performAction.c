5919e50a5609a9a5e3aaedf1f22e25f987caede9
mic1780 <mic1780@users.noreply.github.com>
2014-05-21 14:56:34 -0400

    Fixed issue #2 by making the function isFullCommand

--- performAction.c
+++ performAction.c
@@ -21,8 +21,11 @@
 #include "include/constants.h"
 #include "include/functions.h"
 
-//void ** createHolder(int sock, char *s, int len);
-void destroyHolder(void ** holder, int len);
+int isFullCommand(char * sent);
+int myStrncmp(char *s1, char *s2);
+int getMin(char *s1, char *s2);
+
+//void destroyHolder(void ** holder, int len);
 
 /*
  *	performAction: this function will be used when we want to do
@@ -37,36 +40,41 @@ void *performAction(char *cmd, clientStruct *s) {
 	char fullCmd[1024];
 	void ** holder;
 	
-	if (strncmp(cmd, "test", 4) == 0) {
-		//execute("wshome ; php exec.php test", *s, NULL, NULL, NULL);
-	} else if (strncmp(cmd, "sql", 3) == 0) {
-		strcpy(fullCmd, "php exec.php sql '");
-		strcat(fullCmd, cmd + 4);
-		strcat(fullCmd, "'");
-		
-		//execute(fullCmd, *s, NULL, NULL, NULL);
-	} else if (strncmp(cmd, "set", 3) == 0) {
-		if (strncmp(cmd + 4, "name", 4) == 0) {
-			holder = createISIHolder(getSocket(*s), cmd, 0);
-			s->name = (char *)doFunction("alterStruct", holder);
-			destroyHolder(holder, 3);
-			//s->name =	alterStruct(getSocket(*s), cmd);
-		}//END IF
-	} else if (strncmp(cmd, "sendall", 7) == 0) {
-		sprintf(fullCmd, "Message from %s: %s", getName(*s), cmd+8);
-		holder = createISIHolder(getSocket(*s), fullCmd, strlen(fullCmd));
-		doFunction("sendMessage", holder);
-		destroyHolder(holder, 3);
-		
-		for (i=0; i < NUM_OF_CLIENTS; i++) {
-			if (getActive(*socketArray(i)) == 0 || getSocket(*s) == getSocket(*socketArray(i))) {
-				//do nothing
-			} else {
-				holder = createISIHolder(getSocket(*socketArray(i)), fullCmd, strlen(fullCmd));
-				doFunction("sendMessage", holder);
+	if (isFullCommand(cmd) == 1) {
+		if (strncmp(cmd, "test", 4) == 0) {
+			//execute("wshome ; php exec.php test", *s, NULL, NULL, NULL);
+		} else if (strncmp(cmd, "sql", 3) == 0) {
+			strcpy(fullCmd, "php exec.php sql '");
+			strcat(fullCmd, cmd + 4);
+			strcat(fullCmd, "'");
+			
+			//execute(fullCmd, *s, NULL, NULL, NULL);
+		} else if (strncmp(cmd, "set", 3) == 0) {
+			if (strncmp(cmd + 4, "name", 4) == 0) {
+				holder = createISIHolder(getSocket(*s), cmd, 0);
+				s->name = (char *)doFunction("alterStruct", holder);
 				destroyHolder(holder, 3);
+				//s->name =	alterStruct(getSocket(*s), cmd);
 			}//END IF
-		}//END FOR LOOP
+		} else if (strncmp(cmd, "sendall", 7) == 0) {
+			sprintf(fullCmd, "Message from %s: %s", getName(*s), cmd+8);
+			holder = createISIHolder(getSocket(*s), fullCmd, strlen(fullCmd));
+			doFunction("sendMessage", holder);
+			destroyHolder(holder, 3);
+			
+			for (i=0; i < NUM_OF_CLIENTS; i++) {
+				if (getActive(*socketArray(i)) == 0 || getSocket(*s) == getSocket(*socketArray(i))) {
+					//do nothing
+				} else {
+					holder = createISIHolder(getSocket(*socketArray(i)), fullCmd, strlen(fullCmd));
+					doFunction("sendMessage", holder);
+					destroyHolder(holder, 3);
+				}//END IF
+			}//END FOR LOOP
+		} else {
+			// do nothing! somehow it got in here and it does not belong
+			printf("\n\t **** ERROR: BAD ACTION ATTEMPTED ****\n");
+		}//END IF
 	} else {
 		holder = createISIHolder(getSocket(*s), cmd, strlen(cmd));
 		doFunction("sendMessage", holder);
@@ -75,3 +83,25 @@ void *performAction(char *cmd, clientStruct *s) {
 
 	return NULL;
 }//END FUNCTION
+
+int isFullCommand(char * sent) {
+	if (
+		myStrncmp(sent, "test") == 0 ||
+		myStrncmp(sent, "sendall ") == 0 ||
+		myStrncmp(sent, "set name ") == 0
+	)
+		return 1;
+	else
+		return 0;
+}
+
+int myStrncmp(char *s1, char *s2) {
+	return strncmp(s1, s2, getMin(s1, s2));
+}
+
+int getMin(char *s1, char *s2) {
+	int l1, l2;
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+	return (l1 < l2 ? l1 : l2);
+}
